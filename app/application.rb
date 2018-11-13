13d2adbf6e389b6fa46438c2fbda74615188ace4
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-13 06:24:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,16 @@
 class Application
  
+ 
+ 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      
+      item_name = req.path.split("/items/").last 
+      item = @@items.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
     else
       resp.write "Route not found"
       resp.status = 404
