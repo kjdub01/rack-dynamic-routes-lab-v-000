c5672a0c5a618bccc2270b2e38e083cb52f31c65
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-13 06:40:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,11 +9,11 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
       
-      item = @@items.find{|i| i.name == item_name}
+      if item = @@items.find{|i| i.name == item_name}
  
       resp.write item.price
       
-    elsif @@items.include?(item_name) == false
+    else @@items.include?(item_name) == false
       resp.write "Route not found"
       resp.status = 400
       
