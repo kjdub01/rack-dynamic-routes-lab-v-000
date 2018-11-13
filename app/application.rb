1ad72c45c9d3bf36a39dcc65aee521ee521c3986
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-13 06:23:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      resp.write "You requested the item"
+      
     else
       resp.write "Route not found"
       resp.status = 404
